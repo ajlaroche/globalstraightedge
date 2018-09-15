657933bf0d4f88c2ec62beac6933ce6e840f1205
Antony Laroche <antony@larochefamily.net>
2018-09-15 18:11:54 -0500

    updated server and dependencies

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 const express = require("express");
 const path = require("path");
+const logger = require("morgan");
+const routes = require("./routes");
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+app.use(logger("dev"));
+
+// Define middleware here
+app.use(express.json());  //Replaces body-parser in latest version of express
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
+// Define API routes here
+app.use(routes);
 
 // Send every request to the React app
 // Define any API routes before this runs
