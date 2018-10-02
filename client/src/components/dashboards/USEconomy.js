6e3293503ee49aedc480d965ce0e64c13bae2556
Antony Laroche <antony@larochefamily.net>
2018-10-01 22:15:59 -0500

    added dashboard categories

--- client/src/components/dashboards/USEconomy.js
+++ client/src/components/dashboards/USEconomy.js
@@ -6,13 +6,28 @@ import moment from "moment";
 class USEconomy extends Component {
   render() {
     return (
-      <div className="container-fluid dashboard">
+      <div className="col-lg-8 dashboard">
         <div className="row">
           <h2>US Economy At A Glance</h2>
         </div>
         <div className="row metricsRow">
-          <div className="col-lg-3">
-            <h4>Unemployment</h4>
+          <div className="col-lg-2 metricBox">
+            <h5>Unemployment</h5>
+          </div>
+          <div className="col-lg-2 metricBox">
+            <h5>GDP Growth</h5>
+          </div>
+          <div className="col-lg-2 metricBox">
+            <h5>CPI</h5>
+          </div>
+          <div className="col-lg-2 metricBox">
+            <h5>Productivity</h5>
+          </div>
+          <div className="col-lg-2 metricBox">
+            <h5>Wages</h5>
+          </div>
+          <div className="col-lg-2 metricBox">
+            <h5>Net Trade Balance</h5>
           </div>
         </div>
       </div>
