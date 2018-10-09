162b66bf287571aee2872855d9d3e6ca4aa1cda2
Antony Laroche <antony@larochefamily.net>
2018-10-08 21:16:27 -0500

    added employment section scaffold

---
+++ client/src/components/Sections/Employment.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./Sections.css";
+
+class Employment extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Jobs Report</h2>
+      </div>
+    );
+  }
+}
+
+export default Employment;
--- client/src/pages/USEconomy/USView.js
+++ client/src/pages/USEconomy/USView.js
@@ -2,12 +2,14 @@ import React from "react";
 import Nav from "../../components/Nav";
 import { Container } from "../../components/Grid";
 import USEconomy from "../../components/dashboards";
+import Employment from "../../components/Sections/Employment";
 
 const USView = () => (
   <div>
     <Nav active="US Economy" />
     <Container fluid other="mainBody">
       <USEconomy />
+      <Employment />
     </Container>
   </div>
 );
