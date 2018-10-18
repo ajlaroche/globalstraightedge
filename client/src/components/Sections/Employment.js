132058b3e4f39dfb610dae9df8372860eb2bb90a
Antony Laroche <antony@larochefamily.net>
2018-10-17 21:00:25 -0500

    added gdp section

--- client/src/components/Sections/Employment.js
+++ client/src/components/Sections/Employment.js
@@ -67,7 +67,7 @@ class Employment extends Component {
     return (
       <div className="m-5 px-3">
         <h2>Jobs Report</h2>
-        <div className="row">
+        <section className="row">
           <article className="col-md-8">
             <h4 className="commentary">September Employment Highlights</h4>
             <ul>
@@ -107,7 +107,43 @@ class Employment extends Component {
             </cite>
           </article>{" "}
           <div className="col-md-4" id="payrollChart" />
-        </div>
+        </section>
+
+        <h2>GDP Report</h2>
+        <section className="row">
+          <article className="col-md-8">
+            <h4 className="commentary">September GDP Highlights</h4>
+            <ul>
+              <li>
+                Real gross domestic product (GDP) increased at an annual rate of
+                4.2 percent in the second quarter of 2018, according to the
+                "third" estimate released by the Bureau of Economic Analysis. In
+                the first quarter, real GDP increased 2.2 percent.
+              </li>
+              <li>
+                Real gross domestic product (GDP) increased at an annual rate of
+                4.2 percent in the second quarter of 2018, according to the
+                "third" estimate released by the Bureau of Economic Analysis. In
+                the first quarter, real GDP increased 2.2 percent.
+              </li>
+            </ul>
+            <h4 className="commentary">About the GDP</h4>
+            <p>
+              The value of the goods and services produced in the United States
+              is the gross domestic product. The percentage that GDP grew (or
+              shrank) from one period to another is an important way for
+              Americans to gauge how their economy is doing. The United States'
+              GDP is also watched around the world as an economic barometer.
+            </p>
+            <cite>
+              U.S. Bureau of Labor Statistics, Real Gross Domestic Product,
+              retrieved from
+              <a href="http://fred.stlouisfed.org/series/GDPC1"> FRED</a>
+              {", "}
+              Federal Reserve Bank of St. Louis, October 17, 2018.
+            </cite>
+          </article>
+        </section>
       </div>
     );
   }
